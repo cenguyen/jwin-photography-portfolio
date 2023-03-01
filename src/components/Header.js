c53c78833202bdf68c5405ca612bb0f9a90d8e1f
jnguyen1995 <jon.j.nguyen1995@gmail.com>
2023-02-28 22:05:21 -0500

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+// import components
+import Socials from "./Socials";
+import jwin from "../img/header/jwin.png";
+import MobileNav from "./MobileNav";
+
+//import Link
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex">
+      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        {/* logo */}
+        <Link to={"/"}>
+          <img src={jwin} class="scale-50" alt="" />
+        </Link>
+        {/* nav - initially hidden - show on desktop mode */}
+        <nav className="hidden lg:flex">
+          <Link to={"/"}>Home</Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return <section className="porfolio bg-orange-200">Contact</section>;
 };
 
 export default Contact;
