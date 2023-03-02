9b9b3890632f996075ff172d7213e126d661c329
cenguyen <37422560+cenguyen@users.noreply.github.com>
2023-03-02 15:14:43 -0600

    added socials icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,27 @@
 import React from 'react';
+// import icons
+import { ImTwitter, ImInstagram } from 'react-icons/im';
 
 const Socials = () => {
-  return <div className='hidden xl:flex ml-24'>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href="http://www.twitter.com">
+          <ImTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="http://www.instagram.com">
+          <ImInstagram />
+        </a>
+      </li>
+      <li>
+        <a href="http://www.instagram.com">
+          <ion-icon name="logo-tiktok"></ion-icon>
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
